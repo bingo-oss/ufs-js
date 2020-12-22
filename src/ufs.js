272acdd8d07109f2c6e9d5c4d59e2a93e4817614
yulsh <570519827@qq.com>
2020-12-22 11:05:06 +0800

    兼容startsWith 和 endsWith
    兼容startsWith 和 endsWith

--- src/ufs.js
+++ src/ufs.js
@@ -1,5 +1,18 @@
 import { StorageBase, ConvertBase } from "./base";
 
+// 兼容startsWith和endsWith
+if (typeof String.prototype.startsWith !== 'function') {
+  String.prototype.startsWith = function(prefix) {
+      return this.slice(0, prefix.length) === prefix;
+  };
+}
+
+if (typeof String.prototype.endsWith !== 'function') {
+  String.prototype.endsWith = function(suffix) {
+      return this.indexOf(suffix, this.length - suffix.length) !== -1;
+  };
+}
+
 /**
  * @class StorageClient
  */
